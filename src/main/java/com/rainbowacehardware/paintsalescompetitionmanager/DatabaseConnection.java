1af539cfcfc59026252602d4ade2f9b0309c62f3
Kenny Hector <kennyhector24@gmail.com>
2023-07-21 18:08:15 -0400

    Removing database credentials

--- src/main/java/com/rainbowacehardware/paintsalescompetitionmanager/DatabaseConnection.java
+++ src/main/java/com/rainbowacehardware/paintsalescompetitionmanager/DatabaseConnection.java
@@ -2,24 +2,36 @@ package com.rainbowacehardware.paintsalescompetitionmanager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DatabaseConnection {
 
-    public Connection databaseLink;
-
-    public Connection getConnection(){
+    public Connection getConnection() {
         String databaseName = "rainbowAceHardware";
-        String databaseUser = "kenny";
-        String databasePassword = "123456";
+        String databaseUser = null;
+        String databasePassword = null;
         String url = "jdbc:mysql://localhost/" + databaseName;
 
+        // Load the database credentials from the configuration file or environment variables
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
-        } catch (Exception e){
+            // Load properties from the config.properties file
+            Properties props = new Properties();
+            props.load(getClass().getResourceAsStream("/config.properties"));
+
+            // Load databaseUser and databasePassword from properties
+            databaseUser = props.getProperty("databaseUser");
+            databasePassword = props.getProperty("databasePassword");
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return databaseLink;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url, databaseUser, databasePassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
+
